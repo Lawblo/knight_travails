084f0b775b9f7b796025489481e93f85db8e94da
Thorkil <schjodt.thorkil@gmail.com>
2021-12-27 02:10:53 +0100

    rebuild

--- lib/main.rb
+++ lib/main.rb
@@ -3,15 +3,3 @@
 require_relative 'board'
 require_relative 'knight'
 
-def knight_moves(knight, goal = [0, 0], moves = [], min = 10)
-  return false if moves.any?  { |position| knight.pos == position }
-
-  p knight
-  moves << knight.pos
-  p moves
-
-  return false if moves.length > min
-  return moves if knight.pos == goal
-end
-
-knight_moves(Knight.new([0, 0]), [7, 7])
---
+++ lib/node.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Node
+  def initialize(coord)
+    @coord = coord
+    @up_r = coord
+  end
+end
